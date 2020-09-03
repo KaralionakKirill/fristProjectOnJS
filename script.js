62cbd13043ffd8db6a48287e8b8499a62e12c7f1
Kirill <karalionak_kirill@mail.ru>
2020-09-03 22:36:52 +0300

    add cycle, conditions and function

--- script.js
+++ script.js
@@ -9,9 +9,35 @@ let personalMovieDB = {
     private: false
 };
 
-let firstQuestion = prompt("Один из последних просмотренных фильмов?", ""),
-    secondQuestion = prompt("На сколько оцените его?", "");
+function askQuestions() {
+    for (let i = 0; i < numberOfFilms; i++) {
+        let firstQuestion = prompt("Один из последних просмотренных фильмов?", ""),
+            secondQuestion = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[firstQuestion] = secondQuestion;
+        if (firstQuestion.length == 0 || firstQuestion.length > 50 || firstQuestion == null ||
+            secondQuestion.length == 0 || secondQuestion == null) {
+            console.log("You fucking bitch! Error!");
+            i--;
+        } else {
+            personalMovieDB.movies[firstQuestion] = secondQuestion;
+            console.log("You are nice man!");
+        }
+    }
+}
+
+function checkAnswers() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB <= 30) {
+        console.log("Вы классический зритель");
+    } else if ("Вы киноман") {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+askQuestions();
+checkAnswers();
 
 console.log(personalMovieDB);
